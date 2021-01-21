9576895980cb8c85c7e861ea14296ed5cc1cc927
leandromendes25 <leandromiendes@yahoo.com>
2021-01-21 12:43:22 +0000

    feat: :art: Adicionando grid system ao logo
    Adicionei no component NavBar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,19 +1,36 @@
-import { AppBar, Button, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, makeStyles, Theme, Toolbar, Typography } from '@material-ui/core';
 import * as React from 'react';
 import logo from '../../static/img/logo.png';
 //.. estpa voltando
 //React.FC é o tipo do component. FC = FunctionComponent
 //Typography permite lidar com texto
-const useStyles = makeStyles(styles)
+//Com material-ui podemos criar nosso proprio tema que é a escolha das cores.
+//É bom colocarmos tipo para poder ter o auto complete. Sizes, xs, sm, md, lg e xl  
+const useStyles = makeStyles((theme: Theme) => ({
+    toolbar: {
+        backgroundColor: '#000000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]: { //faz gerar um midia query ao utilizar breakpoint diz quando for maior que o sm(small)
+            width: 170                   //coloca 170
+        }
+
+    }
+}))
 
 
 export const Navbar: React.FC = () => {
+    const classes = useStyles();
     return (
         <AppBar>
-            <Toolbar>
-
-                <Typography>
-                    <img src={logo} alt="codeflix" />
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.title}>
+                    <img src={logo} alt="codeflix" className={classes.logo} />
                 </Typography>
                 <Button color="inherit">Login</Button>
             </Toolbar>
